c878dc8e06f3b547d20ebf6d017cf82b9a049b37
Maks <maxkov19861411@gmail.com>
2024-07-08 17:50:18 +0300

    Updated styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,11 @@ import css from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={css.home}>
+      <p className={css.text}>
+        Looking for a Class C RV rental for your next road trip? Our Class C RV
+        models provide plenty of space and comfort in every setup so you can
+        camp closer to nature with the comforts of home!{' '}
+      </p>
       <img
         className={css.img}
         src="/hanson-lu--Avc2AiE1_Q-unsplash.jpg"
